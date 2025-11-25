91adefe3a64f19278be34223fdb3cb491ed08965
Samuel Garcia Moreira <samuelgarciamoreira1985@gmail.com>
2025-11-25 14:44:52 -0300

    portfolio 25/11/25 14:44 menu

---
+++ portfolio/portfolio-front/src/App.jsx
@@ -0,0 +1,17 @@
+//REACT
+import MainMenu from './components/MainMenu/MainMenu'
+//CSS
+import './App.css'
+
+function App() {
+
+  return (
+
+    <div className='App'>
+      <MainMenu/>
+    </div>
+
+  )
+}
+
+export default App
