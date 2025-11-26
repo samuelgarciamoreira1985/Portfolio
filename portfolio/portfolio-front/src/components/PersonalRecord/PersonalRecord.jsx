20c32c9cda376e6c945b394ec33be945bc4b7548
Samuel Garcia Moreira <samuelgarciamoreira1985@gmail.com>
2025-11-26 16:51:08 -0300

    commit personal record competo

--- portfolio/portfolio-front/src/components/PersonalRecord/PersonalRecord.jsx
+++ portfolio/portfolio-front/src/components/PersonalRecord/PersonalRecord.jsx
@@ -16,15 +16,23 @@ const PersonalRecord = () => {
       </div>
 
       <div className="personal-dev">
-          <h3>Samuel Garcia Moreira</h3>
-          <p>Formado em Análise e Desenvolvimento de Sistemas com foco em desenvolvimento web. Motivado pela paixão a tecnologia, estou sempre buscando por novos desafios e o aperfeiçoamento de minhas habilidades. Atualmente, estou em busca de uma oportunidade no merdado de trabalho na área do desenvolvimento e posso garantir que esforço e dedicação são atributos que jamais irão faltar.</p>
+          <h3 style={{userSelect:"none"}}>Samuel Garcia Moreira</h3>
+          <p style={{userSelect:"none"}}>Formado em Análise e Desenvolvimento de Sistemas com foco em desenvolvimento web. Motivado pela paixão a tecnologia, estou sempre buscando por novos desafios e o aperfeiçoamento de minhas habilidades. Atualmente, estou em busca de uma oportunidade no merdado de trabalho na área do desenvolvimento e posso garantir que esforço e dedicação são atributos que jamais irão faltar.</p>
       </div>
 
       <div className="social-media-dev">
-          <FaLinkedinIn className="icon-social-media-dev"/>
-          <FaGithub  className="icon-social-media-dev"/>
-          <FaInstagramSquare  className="icon-social-media-dev"/>
-          <FaFacebook  className="icon-social-media-dev"/>
+          <a href="https://www.linkedin.com/in/samuel-garcia-moreira-456285308/">
+            <FaLinkedinIn className="icon-social-media-dev"/>
+          </a>
+          <a href="https://github.com/samuelgarciamoreira1985">
+            <FaGithub  className="icon-social-media-dev"/>
+          </a>
+          <a href="https://www.instagram.com/samuel02031985/">
+            <FaInstagramSquare  className="icon-social-media-dev"/>
+          </a>
+          <a href="https://www.facebook.com/samuel.garciamoreira?locale=pt_BR">
+            <FaFacebook  className="icon-social-media-dev"/>
+          </a>
       </div>
 
     </div>
