459b47cb5679da2b3a5ad89bbef1e6871a27c398
Samuel Garcia Moreira <samuelgarciamoreira1985@gmail.com>
2025-11-27 16:32:51 -0300

    commit skills completo

--- portfolio/portfolio-front/src/components/Skills/Skills.jsx
+++ portfolio/portfolio-front/src/components/Skills/Skills.jsx
@@ -3,11 +3,12 @@ import "./Skills.css"
 // ÍCONES
 import { GiSkills } from "react-icons/gi";
 import { FaHtml5,FaDatabase,FaGithub,FaGitAlt } from "react-icons/fa";
-import { SiCss3 } from "react-icons/si";
-import { RiReactjsLine  } from "react-icons/ri";
+import { SiCss3,SiDialogflow } from "react-icons/si";
+import { RiReactjsLine,RiTeamFill } from "react-icons/ri";
 import { IoLogoJavascript } from "react-icons/io5";
 import { FaUnity,FaBrain } from "react-icons/fa6";
 import { PiFileCSharpFill } from "react-icons/pi";
+import { MdDashboardCustomize,MdEmojiPeople } from "react-icons/md";
 
 const Skills = () => {
 
@@ -19,7 +20,7 @@ const Skills = () => {
 
             <div className="title-skills">
                 <GiSkills className="icon-title-skill"/>
-                <span>Minhas Habilidades</span>
+                <span style={{userSelect:"none"}}>Minhas Habilidades</span>
             </div>
 
             <div className="details-skills">
@@ -27,12 +28,12 @@ const Skills = () => {
                 <div className="item-skill">
                     <div className="info-percentage-skills">
                         <FaHtml5 className="icon-skill"/>
-                        <span className="desc-skill">HTML 5</span>
-                        <span className="desc-percentage">90%</span>
+                        <span style={{userSelect:"none"}} className="desc-skill">HTML 5</span>
+                        <span style={{userSelect:"none"}} className="desc-percentage">85%</span>
                     </div>
                     <div className="container-progress-skills">
                         <div className="progress-skills">
-                            <div className="progress-bar" style={{width:"90%"}}>
+                            <div className="progress-bar" style={{width:"85%"}}>
                             </div>
                         </div>
                     </div>
@@ -40,9 +41,9 @@ const Skills = () => {
 
                 <div className="item-skill">
                     <div className="info-percentage-skills">
-                        <SiCss3  className="icon-skill"/>
-                        <span className="desc-skill">CSS 3</span>
-                        <span className="desc-percentage">80%</span>
+                        <SiCss3 className="icon-skill"/>
+                        <span style={{userSelect:"none"}} className="desc-skill">CSS 3</span>
+                        <span style={{userSelect:"none"}} className="desc-percentage">80%</span>
                     </div>
                     <div className="container-progress-skills">
                         <div className="progress-skills">
@@ -55,8 +56,8 @@ const Skills = () => {
                 <div className="item-skill">
                     <div className="info-percentage-skills">
                         <IoLogoJavascript style={{borderRadius:"7px"}} className="icon-skill"/>
-                        <span className="desc-skill">JavaScript</span>
-                        <span className="desc-percentage">75%</span>
+                        <span style={{userSelect:"none"}} className="desc-skill">JavaScript</span>
+                        <span style={{userSelect:"none"}} className="desc-percentage">75%</span>
                     </div>
                     <div className="container-progress-skills">
                         <div className="progress-skills">
@@ -68,23 +69,9 @@ const Skills = () => {
 
                 <div className="item-skill">
                     <div className="info-percentage-skills">
-                        <RiReactjsLine   className="icon-skill"/>
-                        <span className="desc-skill">REACT JS</span>
-                        <span className="desc-percentage">80%</span>
-                    </div>
-                    <div className="container-progress-skills">
-                        <div className="progress-skills">
-                            <div className="progress-bar" style={{width:"80%"}}>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-
-                <div className="item-skill">
-                    <div className="info-percentage-skills">
-                        <FaGitAlt className="icon-skill"/>
-                        <span className="desc-skill">GIT</span>
-                        <span className="desc-percentage">70%</span>
+                        <RiReactjsLine className="icon-skill"/>
+                        <span style={{userSelect:"none"}} className="desc-skill">REACT JS</span>
+                        <span style={{userSelect:"none"}} className="desc-percentage">70%</span>
                     </div>
                     <div className="container-progress-skills">
                         <div className="progress-skills">
@@ -96,9 +83,51 @@ const Skills = () => {
 
                 <div className="item-skill">
                     <div className="info-percentage-skills">
-                        <FaGithub    className="icon-skill"/>
-                        <span className="desc-skill">GITHUB</span>
-                        <span className="desc-percentage">75%</span>
+                        <FaGitAlt className="icon-skill"/>
+                        <span style={{userSelect:"none"}} className="desc-skill">GIT</span>
+                        <span style={{userSelect:"none"}} className="desc-percentage">65%</span>
+                    </div>
+                    <div className="container-progress-skills">
+                        <div className="progress-skills">
+                            <div className="progress-bar" style={{width:"65%"}}>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+
+                <div className="item-skill">
+                    <div className="info-percentage-skills">
+                        <FaGithub className="icon-skill"/>
+                        <span style={{userSelect:"none"}} className="desc-skill">GITHUB</span>
+                        <span style={{userSelect:"none"}} className="desc-percentage">65%</span>
+                    </div>
+                    <div className="container-progress-skills">
+                        <div className="progress-skills">
+                            <div className="progress-bar" style={{width:"65%"}}>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+
+                <div className="item-skill">
+                    <div className="info-percentage-skills">
+                        <FaDatabase className="icon-skill"/>
+                        <span style={{userSelect:"none"}} className="desc-skill">SQL</span>
+                        <span style={{userSelect:"none"}} className="desc-percentage">60%</span>
+                    </div>
+                    <div className="container-progress-skills">
+                        <div className="progress-skills">
+                            <div className="progress-bar" style={{width:"60%"}}>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+
+                <div className="item-skill">
+                    <div className="info-percentage-skills">
+                        <FaUnity className="icon-skill"/>
+                        <span style={{userSelect:"none"}} className="desc-skill">UNITY 3D</span>
+                        <span style={{userSelect:"none"}} className="desc-percentage">75%</span>
                     </div>
                     <div className="container-progress-skills">
                         <div className="progress-skills">
@@ -110,9 +139,9 @@ const Skills = () => {
 
                 <div className="item-skill">
                     <div className="info-percentage-skills">
-                        <FaDatabase    className="icon-skill"/>
-                        <span className="desc-skill">SQL</span>
-                        <span className="desc-percentage">75%</span>
+                        <PiFileCSharpFill className="icon-skill"/>
+                        <span style={{userSelect:"none"}} className="desc-skill">C# PARA JOGOS DIGITAIS</span>
+                        <span style={{userSelect:"none"}} className="desc-percentage">75%</span>
                     </div>
                     <div className="container-progress-skills">
                         <div className="progress-skills">
@@ -124,23 +153,9 @@ const Skills = () => {
 
                 <div className="item-skill">
                     <div className="info-percentage-skills">
-                        <FaUnity     className="icon-skill"/>
-                        <span className="desc-skill">UNITY 3D</span>
-                        <span className="desc-percentage">85%</span>
-                    </div>
-                    <div className="container-progress-skills">
-                        <div className="progress-skills">
-                            <div className="progress-bar" style={{width:"85%"}}>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-
-                <div className="item-skill">
-                    <div className="info-percentage-skills">
-                        <PiFileCSharpFill     className="icon-skill"/>
-                        <span className="desc-skill">C# PARA JOGOS DIGITAIS</span>
-                        <span className="desc-percentage">80%</span>
+                        <FaBrain className="icon-skill"/>
+                        <span style={{userSelect:"none"}} className="desc-skill">LÓGICA DE PROGRAMAÇÃO</span>
+                        <span style={{userSelect:"none"}} className="desc-percentage">80%</span>
                     </div>
                     <div className="container-progress-skills">
                         <div className="progress-skills">
@@ -152,13 +167,55 @@ const Skills = () => {
 
                 <div className="item-skill">
                     <div className="info-percentage-skills">
-                        <FaBrain      className="icon-skill"/>
-                        <span className="desc-skill">LÓGICA DE PROGRAMAÇÃO</span>
-                        <span className="desc-percentage">90%</span>
+                        <SiDialogflow className="icon-skill"/>
+                        <span style={{userSelect:"none"}} className="desc-skill">COMUNICAÇÃO</span>
+                        <span style={{userSelect:"none"}} className="desc-percentage">95%</span>
+                    </div>
+                    <div className="container-progress-skills">
+                        <div className="progress-skills">
+                            <div className="progress-bar" style={{width:"95%"}}>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+
+                <div className="item-skill">
+                    <div className="info-percentage-skills">
+                        <MdDashboardCustomize className="icon-skill"/>
+                        <span style={{userSelect:"none"}} className="desc-skill">ADAPTABILIDADE</span>
+                        <span style={{userSelect:"none"}} className="desc-percentage">85%</span>
+                    </div>
+                    <div className="container-progress-skills">
+                        <div className="progress-skills">
+                            <div className="progress-bar" style={{width:"85%"}}>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+
+                <div className="item-skill">
+                    <div className="info-percentage-skills">
+                        <RiTeamFill className="icon-skill"/>
+                        <span style={{userSelect:"none"}} className="desc-skill">TRABALHO EM EQUIPE</span>
+                        <span style={{userSelect:"none"}} className="desc-percentage">90%</span>
                     </div>
                     <div className="container-progress-skills">
                         <div className="progress-skills">
                             <div className="progress-bar" style={{width:"90%"}}>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+
+                <div className="item-skill">
+                    <div className="info-percentage-skills">
+                        <MdEmojiPeople  className="icon-skill"/>
+                        <span style={{userSelect:"none"}} className="desc-skill">LIDERANÇA</span>
+                        <span style={{userSelect:"none"}} className="desc-percentage">98%</span>
+                    </div>
+                    <div className="container-progress-skills">
+                        <div className="progress-skills">
+                            <div className="progress-bar" style={{width:"98%"}}>
                             </div>
                         </div>
                     </div>
