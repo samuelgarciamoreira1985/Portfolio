931f9b1058d5cf0ca077057dd731ed2f8beb62fd
Samuel Garcia Moreira <samuelgarciamoreira1985@gmail.com>
2025-12-09 12:38:08 -0300

    commit portfolio 09/12 12:38

--- portfolio/portfolio-front/src/components/Formations/Formations.jsx
+++ portfolio/portfolio-front/src/components/Formations/Formations.jsx
@@ -1,3 +1,7 @@
+// REACT
+import { useState, useContext } from "react";
+// CONTEXT
+import { Context_Themes } from '../../context/ThemesContext'
 // CSS
 import "./Formations.css"
 // JSON
@@ -7,22 +11,24 @@ import { FaGraduationCap } from "react-icons/fa6";
 
 const Formations = () => {
 
+    const { colorTheme,setColorTheme } = useContext(Context_Themes)
+
   return (
 
     <div className="container-formations">
             
-        <div className="sub-container-formations">
+        <div className={colorTheme === "dark" ? "sub-container-formations-dark" : "sub-container-formations"}>
 
-            <div className="title-formations">
-                <FaGraduationCap className="icon-title-formations"/>
+            <div className={colorTheme === "dark" ? "title-formations-dark" : "title-formations"}>
+                <FaGraduationCap className={colorTheme === "dark" ? "icon-title-formations-dark" : "icon-title-formations"}/>
                 <span style={{userSelect:"none"}}>Formações</span>
             </div>
 
-            <div className="details-formations">
+            <div className={colorTheme === "dark" ? "details-formations-dark" : "details-formations"}>
 
                 {list_Formations && list_Formations?.map(itemFormation => (
-                <div className="items-formations" key={itemFormation.id}>
-                    <div className="align-items-formation">
+                <div className={colorTheme === "dark" ? "items-formations-dark" : "items-formations"} key={itemFormation.id}>
+                    <div className={colorTheme === "dark" ? "align-items-formation-dark" : "align-items-formation"}>
                         <p style={{fontWeight:"600",fontSize:".9rem"}}>{itemFormation.period}</p>
                         <h4>{itemFormation.institution}</h4>
                         <p>{itemFormation.course}</p>
--- portfolio/portfolio-front/src/components/Projects/Projects.jsx
+++ portfolio/portfolio-front/src/components/Projects/Projects.jsx
@@ -1,3 +1,7 @@
+// REACT
+import { useState, useContext } from "react";
+// CONTEXT
+import { Context_Themes } from '../../context/ThemesContext'
 // CSS
 import "./Projects.css"
 // ÍCONES
@@ -9,34 +13,36 @@ import img_mari_artes from "../../assets/imagens/img-mari-artes.png"
 
 const Projects = () => {
 
+    const { colorTheme,setColorTheme } = useContext(Context_Themes)
+
   return (
 
     <div className="container-projects">
 
-        <div className="sub-container-projects">
+        <div className={colorTheme === "dark" ? "sub-container-projects-dark" : "sub-container-projects"}>
 
-            <div className="title-projects">
-                <FaDiagramProject className="icon-title-projects"/>
+            <div className={colorTheme === "dark" ? "title-projects-dark" : "title-projects"}>
+                <FaDiagramProject className={colorTheme === "dark" ? "icon-title-projects-dark" : "icon-title-projects"}/>
                 <span style={{userSelect:"none"}}>Projetos</span>
             </div>
 
-            <div className="details-projects">
+            <div className={colorTheme === "dark" ? "details-projects-dark" : "details-projects"}>
 
-                <div className="items-projects">
-                    <div className="align-items-projects">
+                <div className={colorTheme === "dark" ? "items-projects-dark" : "items-projects"}>
+                    <div className={colorTheme === "dark" ? "align-items-projects-dark" : "align-items-projects"}>
                         <img src={img_lunch_king} alt="foto do projeto Lunch King" />
-                        <p className="period-project">2025</p>
+                        <p className={colorTheme === "dark" ? "period-project-dark" : "period-project"}>2025</p>
                         <h4>Lunch King - Sistema para Lanchonetes</h4>
-                        <p className="description-project"><IoArrowRedo className="icon-description-project"/> Sistema web com foco específico em serviços para lanchonetes. Possibilita ao usuário realizar novos cadastros, alterações, exclusões e gravações de produtos através de interfaces bonitas e intuitivas. Disponibiliza também uma interface para operações de vendas, permitindo ao operador gerar QrCodes e Notas Fiscais.</p>
+                        <p className={colorTheme === "dark" ? "description-project-dark" : "description-project"}><IoArrowRedo className={colorTheme === "dark" ? "icon-description-project-dark" : "icon-description-project"}/> Sistema web com foco específico em serviços para lanchonetes. Possibilita ao usuário realizar novos cadastros, alterações, exclusões e gravações de produtos através de interfaces bonitas e intuitivas. Disponibiliza também uma interface para operações de vendas, permitindo ao operador gerar QrCodes e Notas Fiscais.</p>
                         <a href="#" target="_blank">Acessar Projeto</a>
                     </div>
                 </div>
-                <div className="items-projects">
-                    <div className="align-items-projects">
+                <div className={colorTheme === "dark" ? "items-projects-dark" : "items-projects"}>
+                    <div className={colorTheme === "dark" ? "align-items-projects-dark" : "align-items-projects"}>
                         <img src={img_mari_artes} alt="foto do projeto Mari-Artes" />
-                        <p className="period-project">2025</p>
+                        <p className={colorTheme === "dark" ? "period-project-dark" : "period-project"}>2025</p>
                         <h4>Mari Artes - Site de Artesanato</h4>
-                        <p className="description-project"><IoArrowRedo className="icon-description-project"/> O site possibilita ao usuário conferir produtos de artesanato assim como seus preços e detalhes específicos. O usuário também tem a possibilidade de adicionar ou remover produtos no seu carrinho de compras e enviar seu pedido finalizado para o destinatário.</p>
+                        <p className={colorTheme === "dark" ? "description-project-dark" : "description-project"}><IoArrowRedo className={colorTheme === "dark" ? "icon-description-project-dark" : "icon-description-project"}/> O site possibilita ao usuário conferir produtos de artesanato assim como seus preços e detalhes específicos. O usuário também tem a possibilidade de adicionar ou remover produtos no seu carrinho de compras e enviar seu pedido finalizado para o destinatário.</p>
                         <a href="#" target="_blank">Acessar Projeto</a>
                     </div>
                 </div>
